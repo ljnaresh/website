0c2e91648d70dd0a9d8ddd19cd936c1def81c904
nareshlakyajagadisha <naresh_lj@outlook.com>
2023-01-15 22:07:41 +0100

    chore: install bootstrap and react bootstrap icons
    Installed bootstrap and imported it into the project using gatsby broweser configuration

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,7 @@
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import './src/styles/global.css';
+import '@fontsource/lato/400.css';
+import '@fontsource/lato/700.css';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/700.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import './src/scss/main.scss';
