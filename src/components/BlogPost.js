5d2209dbfa3839dd66d1229411dd8573bb7d6ef7
nareshlakyajagadisha <naresh_lj@outlook.com>
2023-01-21 00:48:48 +0100

    feat: syntax highligthing
    Uses react syntax higlighter and prism to highlight code in markdown

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import { CalendarFill, ClockFill } from 'react-bootstrap-icons';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+
+import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import * as styles from '../styles/shared.module.css';
 import ReactMarkdown from 'react-markdown';
@@ -30,13 +33,34 @@ const BlogPost = (props) => {
           {post.frontmatter.hero_image_source}
         </a>
       </p>
+
       <ReactMarkdown
+        children={post.body}
         components={{
-          h1: Heading,
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                style={darcula}
+                language={match[1]}
+                PreTag='div'
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+          h1: (props) => <Heading size={1}>{props.children}</Heading>,
+          h2: (props) => <Heading size={2}>{props.children}</Heading>,
+          h3: (props) => <Heading size={3}>{props.children}</Heading>,
+          h4: (props) => <Heading size={4}>{props.children}</Heading>,
+          h5: (props) => <Heading size={5}>{props.children}</Heading>,
+          h6: (props) => <Heading size={6}>{props.children}</Heading>,
         }}
-      >
-        {post.body}
-      </ReactMarkdown>
+      />
     </>
   );
 };
